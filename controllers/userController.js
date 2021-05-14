a02126bb6dc82c703b8b93bcac35d5cbc40fc9f7
ayocodingit <ayocodingit@gmail.com>
2021-05-14 13:05:31 +0700

    change name variable

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,11 +17,11 @@ exports.index = async (req, res) => {
 
 exports.show = async (req, res) => {
     const id = req.params.id
-    const result = await findDataModel(User, id)
-    if (result.error) {
-        return res.status(404).json(result)
+    const model = await findDataModel(User, id)
+    if (model.error) {
+        return res.status(404).json(model)
     }
-    res.json(result);
+    res.json(model);
 }
 
 exports.store = async (req, res) => {
@@ -42,20 +42,20 @@ exports.update = async (req, res) => {
     if (errors) {
         return res.status(422).json(errors)
     }
-    const result = await findDataModel(User, id)
-    if (result.error) {
-        return res.status(404).json(result)
+    const model = await findDataModel(User, id)
+    if (model.error) {
+        return res.status(404).json(model)
     }
-    user = await result.update(data)
+    user = await model.update(data)
     res.json(user);
 }
 
 exports.destroy = async (req, res) => {
     const id = req.params.id
-    const result = await findDataModel(User, id)
-    if (result.error) {
-        return res.status(404).json(result)
+    const model = await findDataModel(User, id)
+    if (model.error) {
+        return res.status(404).json(model)
     }
-    await result.destroy()
+    await model.destroy()
     res.json({message: 'DELETED'});
 }
